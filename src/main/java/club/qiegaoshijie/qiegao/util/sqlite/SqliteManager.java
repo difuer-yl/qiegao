2f9e3402161029dd66a2dc17b90973ed1cdb0cdc
ly <chiaki@difuer.com>
2019-07-24 23:07:49 +0800

    version:0.7.6    优化数据库线程池

--- src/main/java/club/qiegaoshijie/qiegao/util/sqlite/SqliteManager.java
+++ src/main/java/club/qiegaoshijie/qiegao/util/sqlite/SqliteManager.java
@@ -23,6 +23,7 @@ public class SqliteManager
     private String databaseFile;
     private static final int POOLSIZE = 1;
     private Connection[] cpool = new Connection[1];
+    private  Connection connection=null;
     private int cpoolCount = 0;
     private static final Charset UTF8 = Charset.forName("UTF-8");
 
@@ -59,13 +60,24 @@ public class SqliteManager
     {
 
 
-        Connection c = null;
-        if (c == null) {
-            c = DriverManager.getConnection(this.connectionString);
-//            configureConnection(c);
+        try
+        {
+            if ((this.connection != null) && (!this.connection.isClosed())) {
+                return this.connection;
+            }
+            Class.forName("org.sqlite.JDBC");
+            this.connection = DriverManager.getConnection(this.connectionString);
+            return this.connection;
         }
-
-        return c;
+        catch (SQLException ex)
+        {
+            System.out.println("SQLite exception on initialize:" + ex.toString());
+        }
+        catch (ClassNotFoundException ex)
+        {
+            System.out.println("You need the SQLite JBDC library. Google it. Put it in /lib folder.");
+        }
+        return null;
 
 
     }
